5b67cb070d3e0910638dcedaf7542648d19cc0fa
Anton Ozerov <lamerork@gmail.com>
2023-07-17 09:08:15 +0300

    Добавил проверку деления на 0

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ PER_PAGE = '100'
 PERIOD_SJ = 7
 CATALOGUES_SJ = 48
 LANGS = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'C#']
+#LANGS = ['JavaScript']
 
 
 def predict_salary(salary_from, salary_to):
@@ -24,15 +25,19 @@ def predict_salary(salary_from, salary_to):
 
 def get_salaries_hh(vacancies):
 
-    vacancies_processed = 0
+    processed_vacancies = 0
     salary_amount = 0
 
     for vacancy in vacancies:
         if vacancy['salary']:
-            vacancies_processed += 1
+            processed_vacancies += 1
             salary_amount += predict_salary(vacancy['salary']['from'],
                                             vacancy['salary']['to'])
-    return vacancies_processed, int(salary_amount/vacancies_processed)
+    try:
+        average_salary = int(salary_amount/processed_vacancies)
+    except ZeroDivisionError:
+        average_salary = 0
+    return processed_vacancies, average_salary
 
 
 def get_average_salaries_hh(langs):
@@ -45,7 +50,7 @@ def get_average_salaries_hh(langs):
     }
 
     for lang in langs:
-
+        all_vacancies = []
         page = 0
         vacancies_processed = 0
         salary = 0
@@ -67,32 +72,38 @@ def get_average_salaries_hh(langs):
                                     headers=headers)
             response.raise_for_status()
 
-            vacancies = response.json()
-            pages = int(vacancies['pages'])
-            vacancies_processed_page, salary_page = get_salaries_hh(vacancies['items'])
-            vacancies_processed += vacancies_processed_page
-            salary += salary_page
+            page_vacancies = response.json()
+            all_vacancies += [vacancy for vacancy in page_vacancies['items']]
+            pages = int(page_vacancies['pages'])
             page += 1
 
-        statistic_salaries[lang] = {'vacancies_found': vacancies['found'],
+        vacancies_processed, salary = get_salaries_hh(all_vacancies)
+        print(len(all_vacancies))
+        statistic_salaries[lang] = {'vacancies_found': page_vacancies['found'],
                                     'vacancies_processed': vacancies_processed,
-                                    'average_salary': int(salary/pages)}
+                                    'average_salary': salary}
 
     return statistic_salaries
 
 
 def get_salaries_sj(vacancies):
 
-    vacancies_processed = 0
+    processed_vacancies = 0
     salary_amount = 0
 
     for vacancy in vacancies['objects']:
         if vacancy['payment_from'] or vacancy['payment_to']:
-            vacancies_processed += 1
+            processed_vacancies += 1
             salary_amount += predict_salary(vacancy['payment_from'],
                                             vacancy['payment_to'])
 
-    return vacancies_processed, int(salary_amount/vacancies_processed)
+    try:
+        average_salary = int(salary_amount/processed_vacancies)
+    except ZeroDivisionError:
+        average_salary = 0
+    return processed_vacancies, average_salary
+
+    
 
 
 def get_average_salaries_sj(langs, token):
@@ -152,10 +163,10 @@ def main():
     table = AsciiTable(get_list_vacancies(vacancies), title='Head Hunter Moscow')
     print(table.table)
 
-    vacancies = get_average_salaries_sj(LANGS, env.str('SUPERJOB_TOKEN'))
+    #vacancies = get_average_salaries_sj(LANGS, env.str('SUPERJOB_TOKEN'))
 
-    table = AsciiTable(get_list_vacancies(vacancies), title='Super Job Moscow')
-    print(table.table)
+    #table = AsciiTable(get_list_vacancies(vacancies), title='Super Job Moscow')
+    #print(table.table)
 
 
 if __name__ == '__main__':
